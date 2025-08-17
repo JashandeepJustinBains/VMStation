f445e5b4819053c77785915132398812a933b3bb
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2025-08-17 17:18:26 -0400

    Fix deploy.sh to include playbook path in ansible-playbook command

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Run the full site playbook
-ansible-playbook -i ./ansible/inventory.txt --vault-password-file ~/.vault_pass.txt
+ansible-playbook -i ./ansible/inventory.txt --vault-password-file ~/.vault_pass.txt ./ansible/plays/site.yaml
 # ansible-playbook -i ./ansible/inventory.txt ./ansible/plays/site.yaml
