f639167b511d0a33cdcc02ab6b550cbf58d6875d
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2025-08-17 15:57:52 -0400

    Refactor Ansible deployment scripts and security configurations; remove obsolete files and add UFW rules documentation

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# filepath: f:\VMStation\ansible\deploy.sh
-
-# Install required Ansible collections
-ansible-galaxy collection install community.kubernetes
 
 # Run the full site playbook
 ansible-playbook -i ./inventory ./plays/site.yaml
